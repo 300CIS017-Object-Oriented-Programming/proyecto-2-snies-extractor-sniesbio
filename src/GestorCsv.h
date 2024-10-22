ffb5480c6e54807084a8393cf6e0961989ce9a9c
Jeronimo Delvasto <jeronimodelvasto16@javerianacali.edu.co>
2024-10-21 21:14:49 -0500

    FIX:Refactorización y mejoras en la clase GestorCsv

--- src/GestorCsv.h
+++ src/GestorCsv.h
@@ -5,28 +5,23 @@
 #include <vector>
 #include <map>
 #include <fstream>
-// Mantenimiento: Se incluyen librerías que no se utilizan
-#include <sstream>
 #include <list>
-#include <algorithm>
 #include "ProgramaAcademico.h"
 #include "Consolidado.h"
-
-// Recomendación Linter: Se debe evitar el uso de using namespace en archivos de cabecera
-using namespace std;
 
 class GestorCsv
 {
 public:
     GestorCsv() = default;
-    vector<int> leerProgramasCsv(string &ruta);
-    // Mantenimiento: Se puede mejorar la firma y nombre de los metodos para que sea más descriptiva
-    vector<vector<string>> leerArchivoPrimera(string &rutaBase, string &ano, vector<int> &codigosSnies);
-    vector<vector<string>> leerArchivoSegunda(string &rutaBase, string &ano, vector<int> &codigosSnies);
-    vector<vector<string>> leerArchivo(string &rutaBase, string &ano, vector<int> &codigosSnies, int colmunaCodigoSnies);
-    bool crearArchivo(string &ruta, map<int, ProgramaAcademico *> &mapadeProgramasAcademicos, vector<string> etiquetasColumnas);
-    bool crearArchivoBuscados(string &ruta, list<ProgramaAcademico *> &programasBuscados, vector<string> etiquetasColumnas);
-    bool crearArchivoExtra(string &ruta, vector<vector<string>> datosAImprimir);
+
+    std::vector<int> leerProgramasCsv(const std::string &ruta) const;
+    std::vector<std::vector<std::string>> leerArchivoPrimera(const std::string &rutaBase, const std::string &ano, const std::vector<int> &codigosSnies) const;
+    std::vector<std::vector<std::string>> leerArchivoSegunda(const std::string &rutaBase, const std::string &ano, const std::vector<int> &codigosSnies) const;
+    std::vector<std::vector<std::string>> leerArchivo(const std::string &rutaBase, const std::string &ano, const std::vector<int> &codigosSnies, int columnaCodigoSnies) const;
+
+    bool crearArchivo(const std::string &ruta, const std::map<int, ProgramaAcademico *> &mapaProgramas, const std::vector<std::string> &etiquetasColumnas) const;
+    bool crearArchivoBuscados(const std::string &ruta, const std::list<ProgramaAcademico *> &programasBuscados, const std::vector<std::string> &etiquetasColumnas) const;
+    bool crearArchivoExtra(const std::string &ruta, const std::vector<std::vector<std::string>> &datosAImprimir) const;
 };
 
 #endif
